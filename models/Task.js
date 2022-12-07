ecb676a5f6e7fedf0e15fccb226990c5a296ecd8
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-12-06 19:44:08 -0500

    Created Task model and a format Date helper for deadlines.

---
+++ models/Task.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Task extends Model {}
+
+Task.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: (newTaskData) => {
+        newTaskData.deadline = new Date().createDeadline();
+        return newTaskData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "task",
+  }
+);
+
+module.exports = Task;
