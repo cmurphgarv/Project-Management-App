4b1985f534f6d93a7b1ab41650cbda87e69f3f64
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-12-06 20:42:25 -0500

    added user routes and some code to env file

---
+++ controllers/user-routes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const Task = require('../models/Task');
+
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+      const userData = await User.findAll({
+        include: [{ model: Task }],
+      });
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// GET one user by ID
+router.get('/:id', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id, {
+        include: [{ model: Task }],
+      });
+  
+      if (!userData) {
+        res.status(404).json({ message: 'No user found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // CREATE a user
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  
+  module.exports = router;
