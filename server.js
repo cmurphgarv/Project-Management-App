6abfcf2cfe259299411de1f9a0064acc2b9b756a
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-12-08 18:39:33 -0500

    Added a few required packages, and built out skeleton files for future coding.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
