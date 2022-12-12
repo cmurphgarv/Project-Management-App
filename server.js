98dc8f612fe5505bc4eb278503233bdb7716777e
Jared Staples <jstaplesmusic@gmail.com>
2022-12-12 18:50:07 -0500

    Changed .env for secret key

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: "secret-key",
+  secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: false,
 };
