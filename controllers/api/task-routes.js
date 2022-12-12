1d5a9dbd9d50ab4bd14df691b7afd94f6178231b
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-12-12 09:53:09 -0500

    Started working on views, built out needed files, pulled from previous assignments for a starter.

--- controllers/api/task-routes.js
+++ controllers/api/task-routes.js
@@ -1,33 +1,49 @@
 const router = require("express").Router();
 const { Task, Comment, User } = require("../../models");
 
-router.get("/", (req, res) => {
-  Task.findAll().then((taskData) => {
-    res.json(taskData);
-  });
-});
-
-router.get("/:id", (req, res) => {
-  Task.findOne({
-    where: {
-      id: req.params.id,
-    },
-  }).then((taskData) => {
-    res.json(taskData);
-  });
-});
-
-router.post("/", (req, res) => {
-  // create a new task
-  Task.create({
-    id: req.body.id,
-  })
-    .then((newTask) => {
-      res.json(newTask);
-    })
-    .catch((err) => {
-      res.json(err);
+// GET all tasks
+router.get("/", async (req, res) => {
+  try {
+    const userData = await Task.findAll({
+      include: [{ model: User }, { model: Comment }],
     });
+    res.status(200).json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one task by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const userData = await Task.findByPk(req.params.id, {
+      include: [{ model: User }, { model: Comment }],
+    });
+
+    if (!userData) {
+      res.status(404).json({ message: "No user found with that id!" });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// CREATE new task
+
+router.post("/", async (req, res) => {
+  try {
+    const dbTaskData = await Task.create({
+      title: req.body.title,
+      user_id: req.body.user_id,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
