d1f35caf2da386195983ad3b0eab2350c82fc90d
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-12-14 15:48:25 -0500

    logout route on homeroutes.

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -100,4 +100,14 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -5,4 +5,8 @@ const homeRoutes = require("./homeRoute");
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 
+router.use((req, res) => {
+  res.send("<h1>Wrong Route!</h1>");
+});
+
 module.exports = router;
