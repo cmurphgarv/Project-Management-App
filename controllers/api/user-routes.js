ab205f6cda04db153dbc2661d8a19985c986f428
Jared Staples <jstaplesmusic@gmail.com>
2022-12-13 19:40:12 -0500

    Edited homepage GET route to grab only tasks assigned to a user. Edited seed data to hash passwords

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,7 +45,7 @@ router.post("/", async (req, res) => {
     // Set up sessions with a 'loggedIn' variable set to `true`
     req.session.save(() => {
       req.session.loggedIn = true;
-
+      req.session.user_id = dbUserData.id;
       res.status(200).json(dbUserData);
     });
   } catch (err) {
@@ -82,7 +82,7 @@ router.post("/login", async (req, res) => {
     // Once the user successfully logs in, set up the sessions variable 'loggedIn'
     req.session.save(() => {
       req.session.loggedIn = true;
-
+      req.session.user_id = dbUserData.id;
       res
         .status(200)
         .json({ user: dbUserData, message: "You are now logged in!" });
