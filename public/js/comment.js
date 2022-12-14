01b64ac8b5ecc528a187f51fd54421b6bc6502a2
Jared Staples <jstaplesmusic@gmail.com>
2022-12-14 14:36:59 -0500

    Adjusted comment and comment-details handlebars to handle multiple comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,24 +1,25 @@
 const addCommentHandler = async (event) => {
-    event.preventDefault();
-    const commentBox = document.querySelector("#commentBox");
-    const content = commentBox.value.trim();
-    const user_id = commentBox.dataset.userId;
-    const task_id = commentBox.dataset.taskId;
+  event.preventDefault();
+  const commentBox = document.querySelector("#commentBox");
+  const content = commentBox.value.trim();
+  const user_id = commentBox.dataset.userId;
+  const task_id = commentBox.dataset.taskId;
 
-    if (content != "") {
-        const response = await fetch("/api/comments", {
-            method: "POST",
-            body: JSON.stringify({ content, user_id, task_id }),
-            headers: { "Content-Type": "application/json" },
-        });
+  if (content != "") {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ content, user_id, task_id }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-            document.location.replace("/");
-        } else {
-            alert("Failed to post comment.");
-        }
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to post comment.");
     }
+  }
 };
 
-
-document.querySelector(".commentBtn").addEventListener("click", addCommentHandler);
+document
+  .querySelector(".commentBtn")
+  .addEventListener("click", addCommentHandler);
--- public/js/create-comment.js
+++
@@ -1,6 +0,0 @@
-const createComment = async (event) => {
-  event.prevenDefault();
-
-  const content = "";
-  const user_id = req.session.user_id;
-};
