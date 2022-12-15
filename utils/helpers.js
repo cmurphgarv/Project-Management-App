d8648c6d41fcb0886d17b76fb3f20326748badcf
bgilbreath <110920036+bgilbreath@users.noreply.github.com>
2022-12-15 16:38:27 -0500

    Gave up and reverted change on date format. Good enough.

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
   addDays: (date) => {
     let dateCopy = moment(date);
     //console.log(dateCopy);
-    dateCopy = moment(dateCopy).format("MMM Do, YYYY");
+    dateCopy = moment(dateCopy).format("MMM Do YYYY");
     //console.log(dateCopy);
     return dateCopy;
   },
